094f6b361bfe1e51e1bf5d9b6df7844834ef4865
Jaya Dubey <jaya@qutrix.io>
2025-09-18 21:14:51 +0530

    Initial commit adding both NextJS frontend and Laravel backend folders in repository

---
+++ frontend/public/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: "sans-serif", padding: "20px" }}>
+      <h1>Mythology Encyclopedia</h1>
+      <p>Welcome to the world of gods, creatures, and stories!</p>
+    </div>
+  )
+}
